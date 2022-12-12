ab8304aead3510e06306fe85ee8afc1f0728a25d
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-12 18:58:50 +0900

    reducer完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useEffect, useState, useContext, useRef } from "react";
+import { useEffect, useState, useContext, useRef, useReducer } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import KoshiroContext from "./main";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 function App() {
   const [count, setCount] = useState(0);
   const KoshiroInfo = useContext(KoshiroContext);
   const ref = useRef();
+  const [state, dispatch] = useReducer(reducer, 0);
 
   const handleClick = () => {
     setCount(count + 1);
@@ -21,7 +33,6 @@ function App() {
   // useRef
   const handleRef = () => {
     console.log(ref.current.value);
-    console.log(ref.current.offSetHeight);
   };
 
   return (
@@ -39,6 +50,12 @@ function App() {
       <h1>useRef</h1>
       <input type="text" ref={ref} />
       <button onClick={handleRef}>useRef</button>
+
+      <hr />
+      <h1>useReducer</h1>
+      <p>カウント：{state}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
     </div>
   );
 }
