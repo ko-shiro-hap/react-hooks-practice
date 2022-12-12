a0fb96c071296f270f7cb27211d44573231e7523
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-12 18:44:24 +0900

    useRef完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import KoshiroContext from "./main";
@@ -6,6 +6,7 @@ import KoshiroContext from "./main";
 function App() {
   const [count, setCount] = useState(0);
   const KoshiroInfo = useContext(KoshiroContext);
+  const ref = useRef();
 
   const handleClick = () => {
     setCount(count + 1);
@@ -17,6 +18,12 @@ function App() {
     console.log("カウントが更新されました。");
   }, [count]);
 
+  // useRef
+  const handleRef = () => {
+    console.log(ref.current.value);
+    console.log(ref.current.offSetHeight);
+  };
+
   return (
     <div className="App">
       <h1>useState, useEffect</h1>
@@ -27,6 +34,11 @@ function App() {
       <h1>useContext</h1>
       <p>{KoshiroInfo.name}</p>
       <p>{KoshiroInfo.age}</p>
+
+      <hr />
+      <h1>useRef</h1>
+      <input type="text" ref={ref} />
+      <button onClick={handleRef}>useRef</button>
     </div>
   );
 }
